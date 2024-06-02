b3db7bd6644ace41bed0bd823968f55962b42738
rabo8497 <jeon9799@gmail.com>
2024-06-03 04:57:17 +0900

    delete dot env

--- Backend/worldcup_maker/service.py
+++ Backend/worldcup_maker/service.py
@@ -2,10 +2,6 @@ import openai
 import os
 import requests
 import re
-from dotenv import load_dotenv
-
-env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
-load_dotenv(dotenv_path=env_path)
 
 openai_api_key = os.getenv('gptApiKey')
 client = openai.OpenAI(api_key=openai_api_key)
